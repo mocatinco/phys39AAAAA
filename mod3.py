5a570a80a88635f11978ed5dfe1a392c5693ecfb
llsang <luxinsang@brandeis.edu>
2026-09-16 11:40:31 -0400

    Update mod3.py

--- mod3.py
+++ mod3.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-x, y = np.loadtxt("mod3data.txt", unpack=True)
+
+a = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/lab3 phys39.txt')
+
 
 # 2. Plot the data
-plt.plot(x, y, marker="s", linestyle="--", color="r")
-
+#plt.plot(x, y, marker="s", linestyle="--", color="r")
+plt.plot(a.iloc[1:, 1],a.iloc[1:, 0], color='#675c7b')
+plt.show()
 # 3. Add labels and show the plot
 plt.title("Line Plot from TXT File")
 plt.xlabel("Time (s)")
